aaf3a297c23288db510924aa417fb4827aed344b
Sean <shizukesan02@gmail.com>
2025-03-18 11:15:50 -0400

    Pushing initial site
    Pushing a basic react site to soon host all the wonderful code for wretchedrose.com

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Wretched Rose
+        </h1>
+        <h2>
+          Coming 2025 or 2026 ∨ 2027 || 2028 or 2029 or specifically 2034
+        </h2>
+        <h3>
+          
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
