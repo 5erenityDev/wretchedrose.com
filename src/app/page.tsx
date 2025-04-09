257dac407dd79e7cdd29f4cc821288a82dc98fe2
Sean <shizukesan02@gmail.com>
2025-04-09 12:52:08 -0400

    Minor corrections
    Fixing random capitalization and giving the page a more serious tone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
             width={1024}
             height={512}
         />
-        <h2 className="text-[300%] py-10">Please, Come Sit! We have so much to discuss!</h2>
-        <h3 className="text-[100%]">Releasing 2025 or 2026 or 2027 or 2028 or 2029 or specifically 2034</h3>
+        <h2 className="text-[300%] py-10">Please, come sit! We have so much to discuss!</h2>
+        <h3 className="text-[100%]">Development begins soon.</h3>
       </main>
     </div>
   );
